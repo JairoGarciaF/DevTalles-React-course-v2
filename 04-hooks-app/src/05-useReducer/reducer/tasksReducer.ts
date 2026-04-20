3c54a008dcfe754e15474f1a9747d0a832ebc93a
JairoGarciaF <jairo.dgf@hotmail.com>
2026-04-19 19:55:31 -0500

    acabado useReducer

--- 04-hooks-app/src/05-useReducer/reducer/tasksReducer.ts
+++ 04-hooks-app/src/05-useReducer/reducer/tasksReducer.ts
@@ -1,3 +1,5 @@
+import z from "zod";
+
 interface Todo {
   id: number;
   text: string;
@@ -11,10 +13,49 @@ interface TaskState {
   pending: number;
 }
 
+const TodoSchema = z.object({
+  id: z.number(),
+  text: z.string(),
+  completed: z.boolean(),
+});
+
+const TaskStateScheme = z.object({
+  todos: z.array(TodoSchema),
+  length: z.number(),
+  completed: z.number(),
+  pending: z.number(),
+});
+
 export type TaskAction =
   | { type: "ADD_TODO"; payload: string }
   | { type: "TOGGLE_TODO"; payload: number }
   | { type: "DELETE_TODO"; payload: number };
+
+export const getTasksInitialState = (): TaskState => {
+  const localStorageState = localStorage.getItem("tasks-state");
+  if (!localStorageState) {
+    return {
+      todos: [],
+      completed: 0,
+      pending: 0,
+      length: 0,
+    };
+  }
+
+  //  Validar con Zod
+  const result = TaskStateScheme.safeParse(JSON.parse(localStorageState));
+
+  if (result.error) {
+    return {
+      todos: [],
+      completed: 0,
+      pending: 0,
+      length: 0,
+    };
+  }
+
+  return JSON.parse(localStorageState);
+};
 
 export const taskReducer = (
   state: TaskState,
@@ -29,24 +70,37 @@ export const taskReducer = (
       };
       return {
         ...state,
+        length: state.todos.length + 1,
+        pending: state.pending + 1,
         todos: [...state.todos, newTodo],
       };
     }
-    case "TOGGLE_TODO":
+    case "TOGGLE_TODO": {
+      const updatedTodos = state.todos.map((todo) => {
+        if (todo.id === action.payload) {
+          return { ...todo, completed: !todo.completed };
+        }
+        return todo;
+      });
       return {
         ...state,
-        todos: state.todos.map((todo) => {
-          if (todo.id === id) {
-            return { ...todo, completed: !todo.completed };
-          }
-          return todo;
-        }),
+        todos: updatedTodos,
+        completed: updatedTodos.filter((todo) => todo.completed).length,
+        pending: updatedTodos.filter((todo) => !todo.completed).length,
       };
-    case "DELETE_TODO":
+    }
+    case "DELETE_TODO": {
+      const currentTodos = state.todos.filter(
+        (todo) => todo.id !== action.payload,
+      );
       return {
         ...state,
-        todos: state.todos.filter((todo) => todo.id !== action.payload),
+        length: currentTodos.length,
+        todos: currentTodos,
+        completed: currentTodos.filter((todo) => todo.completed).length,
+        pending: currentTodos.filter((todo) => !todo.completed).length,
       };
+    }
     default:
       return state;
   }
--- 04-hooks-app/src/main.tsx
+++ 04-hooks-app/src/main.tsx
@@ -6,12 +6,14 @@ import { TrafficLight } from "./01-useState/TrafficLight";
 import { TrafficLightWithEffect } from "./02-useEffect/TrafficLightWithEffect";
 import { FocusScreen } from "./04-useRef/FocusScreen";
 import { TasksApp } from "./05-useReducer/TaskApp";
+import { ScrambleWords } from "./05-useReducer/ScrambleWords";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     {/* <HooksApp /> */}
     {/* <TrafficLight /> */}
     {/* <TrafficLightWithEffect /> */}
-    <TasksApp />
+    {/* <TasksApp /> */}
+    <ScrambleWords />
   </StrictMode>,
 );
